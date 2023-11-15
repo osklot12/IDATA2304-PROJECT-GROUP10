1508073d551ae9d8e36906c5c6b5dcbc7abbdaa7
Omarboy1 <amermoussa@hotmail.com>
2023-11-15 19:37:55 +0100

    Updated the FieldNodeRunner so it don't have errors

--- src/main/java/no/ntnu/run/FieldNodeRunner.java
+++ src/main/java/no/ntnu/run/FieldNodeRunner.java
@@ -7,6 +7,7 @@ import no.ntnu.network.client.FieldNodeClient;
 public class FieldNodeRunner {
     public static void main(String[] args) {
         // Example usage of FieldNodeClient
+        //Environment environment = new Environment(/* Add required parameters */);
         FieldNode fieldNode = new FieldNode(new Environment());
         FieldNodeClient fieldNodeClient = new FieldNodeClient(fieldNode);
         fieldNodeClient.connect("localhost");
